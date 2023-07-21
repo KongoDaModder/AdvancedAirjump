1aa0d977023b2892ba16f64a7e1ab7791c35ea5e
KongoDaModder <132005160+KongoDaModder@users.noreply.github.com>
2023-07-21 18:32:33 -0400

    Delete SourceCode.cs

--- SourceCode.cs
+++
@@ -1,9 +0,0 @@
-﻿Build started...
-1>------ Build started: Project: Mod 4, Configuration: Debug Any CPU ------
-1>C:\Users\Isaac Mausert\Documents\UnderWaterSpeedBoost\Mod 4\Mod 4\Class1.cs(40,46,40,63): warning CS8622: Nullability of reference types in type of parameter 'sender' of 'void Plugin.OnGameInitialized(object sender, EventArgs e)' doesn't match the target delegate 'EventHandler' (possibly because of nullability attributes).
-1>C:\Users\Isaac Mausert\Documents\UnderWaterSpeedBoost\Mod 4\Mod 4\Class1.cs(14,27,14,33): warning CS8618: Non-nullable field 'sphere' must contain a non-null value when exiting constructor. Consider declaring the field as nullable.
-1>C:\Users\Isaac Mausert\Documents\UnderWaterSpeedBoost\Mod 4\Mod 4\Class1.cs(15,27,15,34): warning CS8618: Non-nullable field 'sphere2' must contain a non-null value when exiting constructor. Consider declaring the field as nullable.
-1>Mod 4 -> C:\Users\Isaac Mausert\Documents\UnderWaterSpeedBoost\Mod 4\Mod 4\bin\Debug\net6.0\Mod 4.dll
-1>Done building project "Mod 4.csproj".
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
-========== Build started at 6:21 PM and took 04.608 seconds ==========
